5c659a2ac5109f32556532a1aa3828d5809108c0
xxie <xxie24@gmail.com>
2016-06-11 11:56:29 -0700

    initial commit

--- xcode_c++/main.cpp
+++ xcode_c++/main.cpp
@@ -7,9 +7,11 @@
 //
 
 #include <iostream>
+#include <regex>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n" << std::endl;
+    
     return 0;
 }
