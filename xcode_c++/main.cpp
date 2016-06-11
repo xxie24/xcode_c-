dbb250625dc3bc39f03b479abe56efdc7bb07f1f
xxie <xxie24@gmail.com>
2016-06-11 11:52:19 -0700

    Initial Commit

---
+++ xcode_c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xcode_c++
+//
+//  Created by XIN XIE on 6/11/16.
+//  Copyright © 2016 XIN XIE. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
